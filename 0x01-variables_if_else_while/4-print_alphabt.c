0b2cd80f0308056ba9c4c6a316a5fe93263de86b
Sharif-tech <asharifsalman@gmail.com>
2022-06-05 16:03:00 -0700

    When I was having that alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * description - pints the alphabet in lowercase, followed by a new line
+ *
+ * return - returns zero
+ *
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0)
+}
